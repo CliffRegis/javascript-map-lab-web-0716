e6460baf884eb590be66c40f7a2d2f27bdad903e
Clifton Regis <github email address>
2016-09-22 08:49:55 -0400

    completed lab

--- index.js
+++ index.js
@@ -11,6 +11,31 @@ const robots = [
   { name: 'Ratchet', alliance: null }
 ];
 
+
+
+
+var tealeaf = 
+  [{ name: 'Bumblebee', alliance: null },
+  { name: 'Laserbeak', alliance: null },
+  { name: 'Barricade', alliance: null },
+  { name: 'Optimus Prime', alliance: null },
+  { name: 'Skywarp', alliance: null },
+  { name: 'Megatron', alliance: null },
+  { name: 'Ironhide', alliance: null },
+  { name: 'Ratchet', alliance: null }]
+
+
+const sortedRobots = Object.assign([], tealeaf);
+  sortedRobots.map(function(robot){
+
+   if (knownDecepticons.includes(robot.name))
+        {robot.alliance = 'decepticon'}
+    else
+      {robot.alliance = 'autobot'}
+
+}); 
+
+
 const zebraStripes = [
   { width: 9.12, color: null },
   { width: 5.71, color: null },
@@ -21,3 +46,23 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null },
 ];
+
+var moon = [
+  { width: 9.12, color: null },
+  { width: 5.71, color: null },
+  { width: 6.01, color: null },
+  { width: 1.54, color: null },
+  { width: 8.34, color: null },
+  { width: 7.77, color: null },
+  { width: 0.59, color: null },
+  { width: 7.31, color: null },
+];
+
+const coloredZebraStripes = Object.assign([], moon)
+coloredZebraStripes.map(function(stripe, index){
+  if (index % 2 == 0)
+        {stripe.color = 'white'}
+    else
+        {stripe.color = 'black'}
+
+});
